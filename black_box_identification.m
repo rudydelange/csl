169ffb5fd39fd86a1e5a6a621b05ec35f3bdb3fa
rudelange <71002023+rudydelange@users.noreply.github.com>
2021-03-03 14:41:55 +0100

    expanded blackbox identification script
    Reads system data from .xlsx files and restructures them in matrices per dataset, per heater and per sensor.
    Reads out time increments for every dataset as well.

--- black_box_identification.m
+++ black_box_identification.m
@@ -1,8 +1,22 @@
 % Control Systems Lab (2020-2021) - Group 3 - Black Box
 close all; clear all; clc;
 %% Read Excel Data
-temp_dat_1 = xlsread('temperature_new_1.xlsx')
-temp_dat_2 = xlsread('temperature_new_2.xlsx')
+temp_dat1 = xlsread('temperature_correct_1.xlsx');
+temp_dat2 = xlsread('temperature_correct_2.xlsx');
+temp_dat3 = xlsread('temperature_correct_3.xlsx');
+
+%% Restructure Data
+% Create matrix for each respective dataset, heater & sensor
+dat1_h1_s1 = [temp_dat1(:,1), temp_dat1(:,3)]; % dataset 1, heater 1, sensor 1
+dat1_h2_s2 = [temp_dat1(:,2), temp_dat1(:,4)]; % ''   '' 1, '' ''  2, '' ''  2
+dat2_h1_s1 = [temp_dat2(:,1), temp_dat2(:,3)];
+dat2_h2_s2 = [temp_dat2(:,2), temp_dat2(:,4)];
+dat3_h1_s1 = [temp_dat3(:,1), temp_dat3(:,3)];
+dat3_h2_s2 = [temp_dat3(:,2), temp_dat3(:,4)];
+% Time increments per dataset
+dat1_t = temp_dat1(2,5); % dataset 1, time increment 
+dat2_t = temp_dat2(2,5);
+dat3_t = temp_dat3(2,5);
 
 %% Open System Identification GUI
-systemIdentification;
+systemIdentification;   % start GUI function
