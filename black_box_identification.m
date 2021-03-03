679e38bc4212373a234e6c327d523bb8aa615543
rudelange <71002023+rudydelange@users.noreply.github.com>
2021-03-03 15:54:58 +0100

    blackbox script altered + arx,armax,bj,oe
    2nd, 4th and 7th order model for ARX, ARMAX, BJ & OE (search method: Trust-Region Reflective Netwon (lsqnonlin))

--- black_box_identification.m
+++ black_box_identification.m
@@ -7,16 +7,16 @@ temp_dat3 = xlsread('temperature_correct_3.xlsx');
 
 %% Restructure Data
 % Create matrix for each respective dataset, heater & sensor
-dat1_h1_s1 = [temp_dat1(:,1), temp_dat1(:,3)]; % dataset 1, heater 1, sensor 1
-dat1_h2_s2 = [temp_dat1(:,2), temp_dat1(:,4)]; % ''   '' 1, '' ''  2, '' ''  2
-dat2_h1_s1 = [temp_dat2(:,1), temp_dat2(:,3)];
-dat2_h2_s2 = [temp_dat2(:,2), temp_dat2(:,4)];
-dat3_h1_s1 = [temp_dat3(:,1), temp_dat3(:,3)];
-dat3_h2_s2 = [temp_dat3(:,2), temp_dat3(:,4)];
+dat1_h12 = [temp_dat1(:,1), temp_dat1(:,2)]; % dataset 1, heater 1, heater 2
+dat1_s12 = [temp_dat1(:,3), temp_dat1(:,4)]; % dataset 1, sensor 1, sensor 2
+dat2_h12 = [temp_dat2(:,1), temp_dat2(:,2)]; % dataset 2, heater 1, heater 2
+dat2_s12 = [temp_dat2(:,3), temp_dat2(:,4)]; % dataset 2, sensor 1, sensor 2
+dat3_h12 = [temp_dat3(:,1), temp_dat3(:,2)]; % dataset 3, heater 1, heater 2
+dat3_s12 = [temp_dat3(:,3), temp_dat3(:,4)]; % dataset 3, sensor 1, sensor 2
 % Time increments per dataset
 dat1_t = temp_dat1(2,5); % dataset 1, time increment 
-dat2_t = temp_dat2(2,5);
-dat3_t = temp_dat3(2,5);
+dat2_t = temp_dat2(2,5); % ''   '' 2, ''    ''    ''
+dat3_t = temp_dat3(2,5); % ''   '' 3, ''    ''    ''
 
 %% Open System Identification GUI
-systemIdentification;   % start GUI function
+systemIdentification;    % start GUI function
