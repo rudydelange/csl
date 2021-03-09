df0047c253cd063113d61c594a63911350cf266c
meesmatthieu <79983324+meesmatthieu@users.noreply.github.com>
2021-03-09 16:55:31 +0100

    beter dan die van eef

--- observermatlab.m
+++ observermatlab.m
@@ -11,32 +11,20 @@ C = Ad.C;
 D = Ad.D;
 
 %% Pole Placement
-%e = x - xhat --> edot = ... = (phi-K*C)*e
+% e = x - xhat --> edot = ... = (phi-K*C)*e
+% det(lambda*eye(4) - (phi - K*C));
 
-syms lambda k1 k2 k3 k4 k5 k6 k7 k8
-K = [k1, k2; k3, k4; k5, k6; k7, k8]; % observer gain
+% Design desired poles characteristic
+% P = (lambda + 1)^4;
+P = [-1, -1, -1.1, -1.1];
 
-eq1 = det(lambda*eye(4) - (phi - K*C));
+% Solve observer gain K for desired Poles P
+K = place(phi.',C.',P).'
 
-% pick eigenvalues: (lambda + 1)^2 <-- poles at p = -1
-P = (lambda + 1)^4;
+%% Check observability 
+ohm = obsv(phi,C);
+observability = length(phi) - rank(ohm); % If zero --> observable
 
-% %% Solve the det eqation for desired observer dynamics
-% eq1_lambda4 = simplify(jacobian(jacobian(eq1, lambda)/2,lambda));
-% 
-% eq1_lambda2 = simplify(jacobian(jacobian(eq1, lambda)/2,lambda));
-% eq1_lambda = simplify(jacobian(eq1-eq1_lambda2*lambda^2, lambda));
-% eq1_rest = simplify(eq1 - eq1_lambda2*lambda^2 - eq1_lambda*lambda);
-% 
-% [k1,k2,k3,k4,k5,k6,k7,k8] = solve(eq1_lambda2 == 1,eq1_lambda == 2, eq1_rest == 1, k1,k2,k3,k4,k5,k6,k7,k8);
-% gives K
-
-P = [-1.1, -1.2, -1.3, -1.4];
-
-%acker(phi,C,P)
-L = place(phi.',C.',P).'
-
-%K_new = [k1, k2; k3, k4; k5, k6; k7, k8]
 %% Observer
 % returns xhat
-%sim('observer')
+sim('observermodel')
