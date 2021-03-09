7c157cda8f5e90f07ff8b1f62fa183346dc3413e
rudelange <71002023+rudydelange@users.noreply.github.com>
2021-03-09 15:05:00 +0100

    pole placement script

--- pole_placement.m
+++ pole_placement.m
@@ -1,31 +1,21 @@
 % Control Systems Lab (2020-2021) - Group 3 - Black Box
 close all; clear all; clc;
 
-%% Load Blackbox Models
-load('blackboxidentification_detrend_2.mat');
+%% Load White-Box Models
+load('matrices.mat');
 
 %% State
-% Retrieve state matrices from BJ model
-[A, B, C, D] = polydata(bj_7th_lsqnonlin);
-% Convert symbolic to double
-% [A] = double([A{:,:}]); % symbolic -> double (cell type)
-% 
-% [B] = double([B{:}]); 
-% 
-% [C] = double([C{:}]);
-% 
-% [D] = double([D{:}]);
-
-
 % Create state space object
-sys = ss(A, B, C, D);
-
+sys = Ad;
+% Retrieve state matrices from BJ model
+A = sys.A;
+B = sys.B;
+C = sys.C;
+D = sys.D;
 % Check open loop eigenvalues
 E = eig(A);
-
 % Desired closed loop eigenvalues
-P = [-2 -1];
-
+P = [-0.98 -0.98 -0.99 -0.99];
 % Solve for K using pole placement
 K = place(A, B, P);
 
@@ -37,16 +27,16 @@ Ecl = eig(Acl);
 syscl = ss(Acl, B, C, D);
 
 % Check step response
-figure(1); hold on;
+figure(1);
 step(sys);
-figure(2); hold on;
+figure(2);
 step(syscl);
 
 % Solve for Kr
 Kdc = dcgain(syscl);
-Kr = 1/Kdc; % inverse of the DC gain that can be used for scaling the system
+Kr = inv(Kdc); % inverse of the DC gain that can be used for scaling the system
 
 % Create scaled input closed
 syscl_scaled = ss(Acl, B*Kr, C, D);
-figure(3); hold on;
+figure(3); 
 step(syscl_scaled);
