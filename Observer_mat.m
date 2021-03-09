5e8afdd9e37836ee6da903162d7f23869951acaa
EvelinePientje <37150618+EvelinePientje@users.noreply.github.com>
2021-03-09 17:59:46 +0100

    Observer update (KUT MEES)

---
+++ Observer_mat.m
@@ -0,0 +1,37 @@
+close all
+clear all
+clc
+
+%% Discrete Matrices
+load('matrices.mat')
+
+phi = Ad.A; % A matrix discrete
+gamma = Ad.B;    % B matrix discrete
+C = Ad.C;
+D = Ad.D;
+
+%% Pole Placement
+P = [-1, -1, -1.1, -1.1];
+
+% Solve observer gain K for desired Poles P
+K = place(phi.',C.',P).';
+
+%% Check observability 
+ohm = obsv(phi,C);
+observability = length(phi) - rank(ohm); % If zero --> observable
+
+%% Observer
+% returns xhat
+
+data = readtable('exceldata21.xlsx');
+data = table2array(data);
+U1 = [data(:,5) data(:,1)];
+U2 = [data(:,5) data(:,2)];
+
+
+y1 = [data(:,5) data(:,3)+273.15];
+y2 = [data(:,5) data(:,4)+273.15];
+tout = data(:,5);
+h = tout(2)-tout(1);
+
+sim('observermodel')
